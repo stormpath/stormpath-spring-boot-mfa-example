32f6829b06b1dabb0a466f8bd0a3e195c79bc9b7
Micah Silverman <micah@afitnerd.com>
2017-02-08 11:44:02 -0500

    Added some static assets

--- src/main/java/com/stormpath/examples/mfa/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/stormpath/examples/mfa/config/SpringSecurityWebAppConfig.java
@@ -24,6 +24,6 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests().antMatchers("/", "/*.png").permitAll();
     }
 }
